06b073757fcebd142d74a058e503930a129b2220
Keisuke Nishimura <keisuke.nishimura@inria.fr>
2026-07-23 18:55:53 +0900

    Fix loop invariants and add definitions

--- general_wp_problems/power.c
+++ general_wp_problems/power.c
@@ -1,12 +1,44 @@
 #include <stdio.h>
+/*@
+ axiomatic a {
+  logic integer fact(integer i);
+  logic integer power(integer i);
+
+  axiom fact_def: fact(0) == 1;
+  axiom power_def: power(0) == 1;
+  }*/
+
+/*@
+requires 0 <= i;
+terminates \true;
+exits \false;
+assigns \nothing;
+ensures \result == power(i);
+*/
+int power(int i);
+
+/*@
+requires 0 <= i;
+terminates \true;
+exits \false;
+assigns \nothing;
+ensures \result == fact(i);
+*/
+int factorial(int i);
+
+/*@
+	requires 0 <= n;
+	assigns \nothing;
+*/
 int pdt(int n) {
   int prod = 1;
   int i = 2;
   /*@
-    loop invariant prod == powr(i/2)*fact(i/2);
+    loop invariant  prod == power((i - 2)/2)*fact((i - 2)/2);
     loop invariant 0 < i;
-    loop invariant i <= n+1;
+    loop invariant i <= n+2;
     loop assigns i, prod;  
+    loop variant n - i;
   */
   while(i < n) {
     prod = power(i/2)*factorial(i/2);
